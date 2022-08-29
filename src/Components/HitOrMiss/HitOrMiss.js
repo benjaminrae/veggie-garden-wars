6a442fc5f098dec86825ebef6b0dabfbff714c29
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-08-29 19:57:51 +0200

    destructure HitOrMiss props

--- src/Components/HitOrMiss/HitOrMiss.js
+++ src/Components/HitOrMiss/HitOrMiss.js
@@ -2,19 +2,19 @@ import "./HitOrMiss.css";
 import miss from "../../assets/gifs/will-ferrell-the-house.gif";
 import hit from "../../assets/gifs/explosion-boom.gif";
 
-const HitOrMiss = (props) => {
+const HitOrMiss = ({ isHit, onContinue }) => {
     return (
         <div className="hit-or-miss">
             <div className="hit-or-miss__screen">
                 <h2 className="hit-or-miss__title">
-                    {props.isHit ? "HIT! 🔥" : "Miss 💔"}
+                    {isHit ? "HIT! 🔥" : "Miss 💔"}
                 </h2>
                 <img
                     className="hit-or-miss__gif"
-                    src={props.isHit ? hit : miss}
+                    src={isHit ? hit : miss}
                     alt=""
                 />
-                <div className="hit-or-miss__button" onClick={props.onContinue}>
+                <div className="hit-or-miss__button" onClick={onContinue}>
                     Continue
                 </div>
             </div>
