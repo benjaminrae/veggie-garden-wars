0742956e128bcb86a09346f35d4835cf329758f4
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-08-29 19:50:43 +0200

    destructure HighScores props

--- src/Components/HighScores/HighScores.js
+++ src/Components/HighScores/HighScores.js
@@ -1,9 +1,9 @@
 import "./HighScores.css";
 
-const HighScores = (props) => {
+const HighScores = ({ onClose }) => {
     return (
         <div className="high-scores">
-            <div className="high-scores__close" onClick={props.onClose}>
+            <div className="high-scores__close" onClick={onClose}>
                 &#10060;
             </div>
             <div className="high-scores__screen">HighScores</div>
